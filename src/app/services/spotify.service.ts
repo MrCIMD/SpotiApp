9a6b75faae6de03f0aabb4471cc0be429fbd8b64
Caleb Isaac Mora Díaz <calexico659@gmail.com>
2020-02-02 12:01:43 -0500

    noimage pipes

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -11,21 +11,21 @@ export class SpotifyService {
     console.log('Spotify service fount');
   }
 
-  public getNewReleases(): any {
+  public getQuery(query: string){
+    const url = `https://api.spotify.com/v1/${ query }`;
+
     const headers = new HttpHeaders({
       Authorization: 'Bearer BQCYWwTETVZtboRstRb-r1yha4bKpJW7jXrBUvDCOUkodgNZcT5VEbJcWxrzaWzyrnv6a53keurpNFSfX-4'
     });
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', {headers}).pipe(map((data: any) => {
-      return data.albums.items;
-    }));
+
+    return this.http.get(url, {headers});
+  }
+
+  public getNewReleases(): any {
+    return this.getQuery('browse/new-releases?limit=20').pipe(map((data: any) => data.albums.items));
   }
 
   public getArtist(text: string){
-    const headers = new HttpHeaders({
-      Authorization: 'Bearer BQCYWwTETVZtboRstRb-r1yha4bKpJW7jXrBUvDCOUkodgNZcT5VEbJcWxrzaWzyrnv6a53keurpNFSfX-4'
-    });
-    return this.http.get(`https://api.spotify.com/v1/search?q=${text}&type=artist&limit=20`, {headers}).pipe(map((data: any) => {
-      return data.artists.items;
-    }));
+    return this.getQuery(`search?q=${text}&type=artist&limit=20`).pipe(map((data: any) => data.artists.items));
   }
 }
