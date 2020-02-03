61f3b13b9c83e2f3c9907ecbde5bf29db7166b3e
Caleb Isaac Mora Díaz <calexico659@gmail.com>
2020-02-02 22:01:58 -0500

    App terminada sin tokes automaticos

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,12 +10,19 @@ export class HomeComponent {
 
   public newReleases: any[] = [];
   public loading: boolean;
+  public error: any;
 
   constructor(private spotify: SpotifyService) {
+    this.error = {
+      exits: false
+    };
     this.loading = true;
     this.spotify.getNewReleases().subscribe((data: any) => {
       this.newReleases = data;
       this.loading = false;
+    }, error => {
+      error.error.error.exits = true;
+      this.error = error.error.error;
     });
   }
 
